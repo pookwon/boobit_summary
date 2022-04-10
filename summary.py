d2657056445d05adf9b5e6381222594f67dec7bc
pookwon <pookwon@gmail.com>
2022-04-10 22:23:24 +0900

    fix : coin market empty value

--- summary.py
+++ summary.py
@@ -4,8 +4,14 @@ import pandas as pd
 import operator
 
 
+def get_coin_price(source, cointype, date):
+    market = cointype + "USD"
+    user_trades = source[ (source['거래'] == market) & (source['날짜'] >= date) & (source['체결 코인 수량'] == cointype) & (source['거래 평균가'] > 0)]
+    #print(user_trades)
+    return user_trades["거래 평균가"].values[0]
+
 def summary_voulme(source_path, target_path):
-    source_frames = pd.read_csv(source_path, dtype={'날짜':str, '유저ID':str, '코인 종류':str, '체결 수량':float64, '거래 평균가':float64, '수수료':float64})
+    source_frames = pd.read_csv(source_path, dtype={'날짜':str, '종류':str, '거래':str, '유저ID':str, '체결 수량':float64, '체결 코인 수량':str, '거래 평균가':float64, '수수료':float64})
 
     #source_path = './sample.xlsx'
     #source_frames = pd.read_excel(source_path)
@@ -35,24 +41,39 @@ def summary_voulme(source_path, target_path):
         uid_fee_total = 0.0
         for index, item in user_trades.iterrows():
             #print(item)
-            #print('avg', item[13], 'qty', item[11])
 
+            trade_date = item['날짜']
             avg_price = float(item['거래 평균가'])
             qty = float(item['체결 수량'])
             fees = float(item['수수료'])
-            coin_type = item['코인 종류']
-
-            if qty > 0 and avg_price > 0:
-                uid_total += avg_price * qty
+            coin_type = item['체결 코인 수량']
+            market = item["종류"]
+            trade_market = item['거래']
+            if fees != 0:
                 if coin_type == 'USDT':
                     uid_fee_total += fees
                 else:
-                    uid_fee_total += avg_price * fees
-        
-        uid_datas.append({'UID':str(uid), '거래량':int(uid_total), '수수료':-uid_fee_total})
+                    if market == 'COIN 선물':
+                        market_determine = coin_type + 'USD'
+                        if market_determine == trade_market:
+                            if avg_price > 0: 
+                                uid_fee_total += avg_price * fees
+                                #print(trade_market, coin_type, avg_price, fees)
+                            else:
+                                avg_price = get_coin_price(source_frames, coin_type, trade_date)
+                                uid_fee_total += avg_price * fees
+                                #print(trade_market, coin_type, avg_price, fees)
+                        else:
+                            avg_price = get_coin_price(source_frames, coin_type, trade_date)
+                            uid_fee_total += avg_price * fees
+                            #print(trade_market, coin_type, avg_price, fees)
+                    else:
+                        uid_fee_total += fees                            
+                                
+        uid_datas.append({'UID':str(uid), '수수료':-uid_fee_total})
 
-    uid_datas.sort(key=operator.itemgetter('거래량'), reverse=True)
+    uid_datas.sort(key=operator.itemgetter('수수료'), reverse=True)
 
-    print(uid_datas)
+    #print(uid_datas)
     sort_userdata = pd.DataFrame(uid_datas)
     sort_userdata.to_excel(target_path)
